4463fedbbad6b7f6e0f4240129cd4b8824eb0c60
Max <maximiliano.santiago@gmail.com>
2010-11-22 19:06:56 -0200

    Rails Enviroment Configuration

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyWeight::Application.config.secret_token = '66efb76462cbf046ddefd8ec3147ace362b8d7d27a2a886f82c66c4dacc0567628063fa4b9132c3774b7f67c62b1764f298b12656a46fd10743a98ec3a637e17'
