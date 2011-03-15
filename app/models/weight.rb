37d47dd0772e201d0be443233002db322e101c44
Max <maximiliano.santiago@gmail.com>
2011-03-15 18:50:34 -0300

    Fixing date validation

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -1,5 +1,4 @@
 class Weight < ActiveRecord::Base
   validates :weight,  :presence => true
   validates :weight, :numericality => {:greater_than_or_equal_to => 0.01}
-  validates :date, :presence => true
 end
