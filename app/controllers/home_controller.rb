e005ac74f009c26d59fc213665b5c7e46bd8b360
Max M2M <max.santiago@m2msolutions.com.br>
2012-08-14 14:14:48 -0300

    refactoring main query

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
   def index
     @weight = Weight.new
-    @weights = Weight.find(:all, :conditions => ["user_id = ?", current_user.id], :order => "date DESC")
+    @weights = current_user.weights.order("date DESC")
   end
 
 end
