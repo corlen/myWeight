50f3c31008582a51845059ae0e8dd0a151e67acb
Max M2M <max.santiago@m2msolutions.com.br>
2012-08-17 14:11:43 -0300

    Adding profile editing, Weights CRUD e some cleanup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+  
+ def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+    end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to(user_profile_index_path, :notice => 'User was successfully updated.') }
+      else
+        format.html { render :action => "edit" }
+      end
+    end
+  end
+  
+  
+    def update_password
+    @user = User.find(current_user.id)
+    if @user.update_attributes(params[:user])
+      # Sign in the user by passing validation in case his password changed
+      sign_in @user, :bypass => true
+      redirect_to root_path
+    else
+      render "edit"
+    end
+  end
+
+  
+
+end
